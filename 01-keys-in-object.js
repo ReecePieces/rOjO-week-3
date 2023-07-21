d4f0f7c01103af48b0a654ca2a69ee5d1f1c5c2b
Maurice <curtis.virges@gmail.com>
2023-07-21 00:58:54 -0400

    first save

--- 01-keys-in-object.js
+++ 01-keys-in-object.js
@@ -13,8 +13,14 @@ keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
 function keysInObject(obj) {
-  // Your code here
+  for (let key in obj) {
+    console.log(key)
+  }
 }
-i been having dreams
+let animals = { dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo' }
+let foods = { apple: 'tart', lemon: 'sour', mango: 'sweet' }
+keysInObject(animals); // => ["dog", "cat", "bison"]
+keysInObject(foods); // => ["apple", "lemon", "mango"]
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keysInObject;
